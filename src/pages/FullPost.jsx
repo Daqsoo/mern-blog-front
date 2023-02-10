497472063edd4f3fa1953289fa976876984c1c6a
Daqsoo <Mr.Spy710@gmail.com>
2023-02-10 17:02:51 +0200

    react router,language change

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -34,14 +34,14 @@ export const FullPost = () => {
         items={[
           {
             user: {
-              fullName: "Вася Пупкин",
+              fullName: "Andrew Lincoln",
               avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
             },
-            text: "Это тестовый комментарий 555555",
+            text: "Test comment",
           },
           {
             user: {
-              fullName: "Иван Иванов",
+              fullName: "Ronald MacClung",
               avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
             },
             text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,8 @@ export const Home = () => {
   return (
     <>
       <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
-        <Tab label="Новые" />
-        <Tab label="Популярные" />
+        <Tab label="New" />
+        <Tab label="Popular Right Now" />
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
@@ -26,7 +26,7 @@ export const Home = () => {
                   'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
                 fullName: 'Keff',
               }}
-              createdAt={'12 июня 2022 г.'}
+              createdAt={'10.02.2023'}
               viewsCount={150}
               commentsCount={3}
               tags={['react', 'fun', 'typescript']}
@@ -35,19 +35,19 @@ export const Home = () => {
           ))}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={['react', 'typescript', 'tags']} isLoading={false} />
           <CommentsBlock
             items={[
               {
                 user: {
-                  fullName: 'Вася Пупкин',
+                  fullName: 'Andrew Lincoln',
                   avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
                 },
-                text: 'Это тестовый комментарий',
+                text: 'Test comment',
               },
               {
                 user: {
-                  fullName: 'Иван Иванов',
+                  fullName: 'Ronald MacClung',
                   avatarUrl: 'https://mui.com/static/images/avatar/2.jpg',
                 },
                 text: 'When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top',
